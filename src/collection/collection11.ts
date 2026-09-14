229bb6e3bf40226d6b5f184f6d700643f1922f09
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-14 18:39:21 +0600

    collection11

--- src/collection/collection11.ts
+++ src/collection/collection11.ts
@@ -2,6 +2,22 @@
 	Создайте функцию groupBy, которая принимает массив объектов и ключ, возвращает Map, где ключи - значения этого свойства, а значения - массивы объектов с таким значением свойства.
 */
 
-export function groupBy<T extends Record<string, any>>(arr: T[], key: keyof T): Map<T[keyof T], T[]> {
+export function groupBy<T extends Record<string, any>>(
+  arr: T[],
+  key: keyof T
+): Map<T[keyof T], T[]> {
+  const result = new Map<T[keyof T], T[]>();
 
+  arr.forEach((item) => {
+    const value = item[key];
+
+    if (!result.has(value)) {
+      result.set(value, []);
+    }
+
+    result.get(value)!.push(item);
+  });
+
+  return result;
 }
+
