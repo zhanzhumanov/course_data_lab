641f1da663ab8fa8f5e8f6372188f15df0dd6db9
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-14 18:42:47 +0600

    collection12

--- src/collection/collection12.ts
+++ src/collection/collection12.ts
@@ -4,7 +4,18 @@
 
 export function cache<T extends (...args: any[]) => any>(fn: T): T {
   const cacheMap = new Map<string, any>();
-  return function(this: any, ...args: any[]) {
 
+  return function(this: any, ...args: any[]) {
+    const key = JSON.stringify(args);
+
+    if (cacheMap.has(key)) {
+      return cacheMap.get(key);
+    }
+
+    const result = fn.apply(this, args);
+    cacheMap.set(key, result);
+
+    return result;
   } as T;
 }
+
