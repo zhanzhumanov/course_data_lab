28ad3774558a91f11145f94ef2cb6f521c8ff4fd
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-14 18:02:52 +0600

    collection09

--- src/collection/collection09.ts
+++ src/collection/collection09.ts
@@ -1,9 +1,20 @@
-/* 
-	Напишите функцию findIntersection, которая принимает два массива и возвращает массив их общих элементов, используя Set.
+import { describe, it, expect } from 'vitest';
+import { findIntersection } from './collection09';
+
+describe('findIntersection', () => {
+  it('should find intersection of two arrays', () => {
+    expect(findIntersection([1, 2, 3, 4], [3, 4, 5, 6])).toEqual([3, 4]);
+    expect(findIntersection([1, 2], [3, 4])).toEqual([]);
+  });
+});
+
+/*
+  Напишите функцию findIntersection, которая принимает два массива
+  и возвращает массив их общих элементов, используя Set.
 */
 
 export function findIntersection(arr1: number[], arr2: number[]): number[] {
-  const intersection = new Set<number>();
+  const set2 = new Set(arr2);
 
-  return [...intersection];
+  return [...new Set(arr1.filter((item) => set2.has(item)))];
 }
