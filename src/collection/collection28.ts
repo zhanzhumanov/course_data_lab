d7cbfe79e6270c46d00579da383e97d2facaf7df
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 19:14:26 +0600

    collection28

--- src/collection/collection28.ts
+++ src/collection/collection28.ts
@@ -5,5 +5,8 @@
 type Product = {category: string, rating: number, name: string}
 
 export function getTopRatedInCategory(products: Product[], category: string): Product[] {
-
+ return products
+    .filter(product => product.category === category)
+    .sort((a, b) => b.rating - a.rating)
+    .slice(0, 3);
 }
