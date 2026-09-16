69b756acf87b73b465874c9c34584dd84878f2a2
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 18:27:29 +0600

    collection19

--- src/collection/collection19.ts
+++ src/collection/collection19.ts
@@ -9,7 +9,9 @@ type User = {
 };
 
 export function getTopAdultUsers(users: User[]): User[] {
-
+return users
+    .filter(user => user.age > 18 && user.rating > 4.5)
+    .sort((a, b) => a.name.localeCompare(b.name));
 }
 
 
