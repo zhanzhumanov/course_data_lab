c7d45ceaea1ce9ce9677744f6c8805070e63fe18
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 16:05:25 +0600

    collection17

--- src/collection/collection17.ts
+++ src/collection/collection17.ts
@@ -5,6 +5,8 @@
 export type User = {email: string, verified: boolean};
 
 export function getVerifiedEmails(users: User[]): string[] {
-	
+	return users
+    .filter(user => user.verified)
+    .map(user => user.email);
 }
 
