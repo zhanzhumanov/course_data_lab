595cd99b376b56983843fd04df3faae9e1ed27cb
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 18:51:17 +0600

    collection21

--- src/collection/collection21.ts
+++ src/collection/collection21.ts
@@ -5,5 +5,7 @@
 export type Product = { price: number; discount: boolean; inStock: boolean };
 
 export function getDiscountedInStockTotal(products: Product[]): number {
-
+ return products
+    .filter(product => product.discount && product.inStock)
+    .reduce((total, product) => total + product.price, 0);
 }
