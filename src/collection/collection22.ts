8e358093d3bcbca5c8927613cc6db9663e06a71e
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 18:52:52 +0600

    collection22

--- src/collection/collection22.ts
+++ src/collection/collection22.ts
@@ -3,7 +3,9 @@
 */
 
 export function sumProperty<T>(objects: T[], property: keyof T): number {
-	
+	return objects.reduce((sum, object) => {
+    return sum + Number(object[property]);
+  }, 0);
 }
 
 
