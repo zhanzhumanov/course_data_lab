379d6c201667e15339c0f1d6057d06dc64f15e38
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 19:17:21 +0600

    collection29

--- src/collection/collection29.ts
+++ src/collection/collection29.ts
@@ -5,5 +5,18 @@
 type User = {active: boolean, progress: number}
 
 export function calculateTotalProgress(users: User[]): number {
+const filteredUsers = users.filter(
+    user => user.active && user.progress > 50
+  );
 
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  const total = filteredUsers.reduce(
+    (sum, user) => sum + user.progress,
+    0
+  );
+
+  return total / filteredUsers.length;
 }
