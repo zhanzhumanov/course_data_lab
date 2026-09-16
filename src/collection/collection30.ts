bb922f9d4cbe82c467f40904db1c4329bcc38c64
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 19:18:46 +0600

    collection30

--- src/collection/collection30.ts
+++ src/collection/collection30.ts
@@ -5,7 +5,9 @@
 type User = {email: string, verified: boolean, age: number}
 
 export function getVerifiedAdultEmails(users: User[]): string[] {
-	
+	return users
+    .filter(user => user.verified && user.age > 18)
+    .map(user => user.email);
 }
 
 
