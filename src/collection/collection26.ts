86c3d9173857cf17ef2c99edd2ab7798391371a5
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 19:04:51 +0600

    collection26

--- src/collection/collection26.ts
+++ src/collection/collection26.ts
@@ -6,5 +6,22 @@ type Product = {name: string, price: number};
 type GroupedProducts = {cheap: Product[], medium: Product[], expensive: Product[]};
 
 export function groupProductsByPrice(products: Product[]): GroupedProducts {
+return products.reduce(
+    (groups, product) => {
+      if (product.price < 1000) {
+        groups.cheap.push(product);
+      } else if (product.price <= 5000) {
+        groups.medium.push(product);
+      } else {
+        groups.expensive.push(product);
+      }
 
+      return groups;
+    },
+    {
+      cheap: [],
+      medium: [],
+      expensive: []
+    }
+  );
 }
