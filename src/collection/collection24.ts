d7a46d3803f01ca567eb78c384ccaca4014a0c37
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 19:01:46 +0600

    collection24

--- src/collection/collection24.ts
+++ src/collection/collection24.ts
@@ -6,7 +6,17 @@ export function findMaxWithCondition<T>(
 	array: T[], 
 	propertyName: keyof T,
 	condition: (item: T) => boolean): T | null {
+const filtered = array.filter(condition);
 
+  if (filtered.length === 0) {
+    return null;
+  }
+
+  return filtered.reduce((max, item) => {
+    return Number(item[propertyName]) > Number(max[propertyName])
+      ? item
+      : max;
+  });
 }
 
 
