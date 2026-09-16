dad73ea6286eeb4d7825af7d394c15cedce9e316
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 19:03:49 +0600

    collection25

--- src/collection/collection25.ts
+++ src/collection/collection25.ts
@@ -6,5 +6,20 @@ type Order = {status: string, amount: number}
 type Stats = {total: number, count: number, average: number}
 
 export function getOrderStats(orders: Order[]): Stats{
+  const completedOrders = orders.filter(order => order.status === 'completed');
 
+  const total = completedOrders.reduce(
+    (sum, order) => sum + order.amount,
+    0
+  );
+
+  const count = completedOrders.length;
+
+  const average = count === 0 ? 0 : total / count;
+
+  return {
+    total,
+    count,
+    average
+  };
 }
