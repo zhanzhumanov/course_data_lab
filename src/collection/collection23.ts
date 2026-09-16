6c7f0ad2120b30a65d18ff8da48a75cc27a260f5
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 18:54:48 +0600

    collection23

--- src/collection/collection23.ts
+++ src/collection/collection23.ts
@@ -3,7 +3,17 @@
 */
 
 export function groupByCategory<T extends Record<string, any>>(arr: T[], key: keyof T): Record<T[keyof T], T[]> {
+return arr.reduce((groups, item) => {
+    const category = item[key];
 
+    if (!groups[category]) {
+      groups[category] = [];
+    }
+
+    groups[category].push(item);
+
+    return groups;
+  }, {} as Record<T[keyof T], T[]>);
 }
 
 
