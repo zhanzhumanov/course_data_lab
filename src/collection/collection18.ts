3a4833cf0259cd0da5eaa39ed2a710b5b3475494
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 18:23:54 +0600

    collection18

--- src/collection/collection18.ts
+++ src/collection/collection18.ts
@@ -5,5 +5,7 @@
 type Product = {name: string, price: number, inStock: boolean};
 
 export function getAffordableInStockProducts(products: Product[]): string[] {
-
+return products
+    .filter(product => product.price < 1000 && product.inStock)
+    .map(product => product.name);
 }
