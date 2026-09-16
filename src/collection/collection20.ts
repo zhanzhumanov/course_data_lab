37bdbc4ccd860bf4cfc9e1f608610100c4ae49d0
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-16 18:49:45 +0600

    collection20

--- src/collection/collection20.ts
+++ src/collection/collection20.ts
@@ -5,7 +5,21 @@
 export type Task = { category: string; priority: string; completed: boolean };
 
 export function groupUnfinishedHighPriorityTasks(tasks: Task[]): Map<string, Task[]> {
+const result = new Map<string, Task[]>();
 
+  const filtered = tasks.filter(
+    task => task.completed === false && task.priority === 'high'
+  );
+
+  filtered.forEach(task => {
+    if (!result.has(task.category)) {
+      result.set(task.category, []);
+    }
+
+    result.get(task.category)?.push(task);
+  });
+
+  return result;
 }
 
 
