b16751f6ebcc7f70d5de045bdfafb03a3fe95817
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-17 13:57:17 +0600

    mongo02

--- src/mongo/mongo02.ts
+++ src/mongo/mongo02.ts
@@ -14,4 +14,6 @@ export class Student {
 export async function find_students_by_grade(db: Db, grade: string): Promise<Student[]> {
     // TODO: Найти всех студентов с указанным классом
 	return db.collection("students")
+    .find({ grade })
+        .toArray() as Student[]
 }
