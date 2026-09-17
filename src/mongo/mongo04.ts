4acce1bd633ea7189acd27cb9061ece284310182
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-17 14:47:10 +0600

    mongo04

--- src/mongo/mongo04.ts
+++ src/mongo/mongo04.ts
@@ -13,5 +13,7 @@ export class Product {
 
 export async function remove_products_by_category(db: Db, category: string) {
     // TODO: Удалить все продукты указанной категории
-	await db.collection("products")
+	await db.collection("products").deleteMany({
+        category: category
+    })
 }
