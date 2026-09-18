aec1a7eb7bfe7ee588a1f67bb16e691a989f9910
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:20:00 +0600

    08

--- src/prisma/prisma08.ts
+++ src/prisma/prisma08.ts
@@ -10,4 +10,20 @@ export interface CourseWithStudentCount {
 export async function find_courses_with_student_count(): Promise<CourseWithStudentCount[]> {
     // TODO: Найти все курсы с количеством уникальных студентов, имеющих оценки по этому курсу
     // Вернуть массив курсов с дополнительным полем studentCount
+    const courses = await prisma.course.findMany({
+        include: {
+            _count: {
+                select: {
+                    grades: true
+                }
+            }
+        }
+    })
+
+    return courses.map(course => ({
+        id: course.id,
+        title: course.title,
+        description: course.description,
+        studentCount: course._count.grades
+    }))
 }
