b88b29e6c322a9a2e0d48da0b342028c2a8c92c9
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:32:47 +0600

    26

--- src/prisma/prisma26.ts
+++ src/prisma/prisma26.ts
@@ -3,4 +3,19 @@ import { prisma } from './prisma_init'
 export async function find_oldest_and_newest_students() {
     // TODO: Найти самого старого и самого нового студента по дате создания
     // Вернуть объект { oldest: студент, newest: студент } с информацией о person
+const students = await prisma.student.findMany({
+        include: {
+            person: true
+        }
+    })
+
+    const oldest = students.reduce((oldest, student) =>
+        student.person.createdAt < oldest.person.createdAt ? student : oldest
+    )
+
+    const newest = students.reduce((newest, student) =>
+        student.person.createdAt > newest.person.createdAt ? student : newest
+    )
+
+    return { oldest, newest }
 }
