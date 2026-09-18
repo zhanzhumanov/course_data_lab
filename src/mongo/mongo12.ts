01fcb7ceaef48beb51fe02531e54811bdd0d7d46
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 14:22:17 +0600

    12

--- src/mongo/mongo12.ts
+++ src/mongo/mongo12.ts
@@ -13,5 +13,12 @@ export class BlogPost {
 
 export async function find_posts_by_keyword(db: Db, keyword: string): Promise<BlogPost[]> {
     // TODO: Найти посты, в заголовке или содержании которых есть указанное ключевое слово
-	return db.collection("posts")
+	 return await db.collection<BlogPost>("posts")
+        .find({
+            $or: [
+                { title: { $regex: keyword } },
+                { content: { $regex: keyword } }
+            ]
+        })
+        .toArray()
 }
