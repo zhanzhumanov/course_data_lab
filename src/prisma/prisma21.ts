8f3510ca61261a3805122885290a0c08a10dd59f
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:31:53 +0600

    21

--- src/prisma/prisma21.ts
+++ src/prisma/prisma21.ts
@@ -4,4 +4,14 @@ export async function find_students_without_courses() {
     // TODO: Найти всех студентов, у которых нет ни одного курса (оценок)
     // Использовать фильтрацию средствами Prisma (none)
     // Вернуть массив студентов с информацией о person
+ return await prisma.student.findMany({
+        where: {
+            grades: {
+                none: {}
+            }
+        },
+        include: {
+            person: true
+        }
+    })
 }
