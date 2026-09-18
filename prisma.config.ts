5533fb69870b832cf1c4c6f91d5aa03e313e58a7
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:35:31 +0600

    newenv

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -8,6 +8,6 @@ export default defineConfig({
   },
   engine: "classic",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL!,
   },
 });
