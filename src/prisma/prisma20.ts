2f79852bf0e05fccfa8bff3c2df9cf6f7bd23ed5
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:27:26 +0600

    20

--- src/prisma/prisma20.ts
+++ src/prisma/prisma20.ts
@@ -4,4 +4,26 @@ export async function update_student_names_pattern(oldPattern: string, newPatter
     // TODO: Обновить имена студентов, заменяя oldPattern на newPattern
     // Использовать обработку в TypeScript с отдельными запросами update
     // Вернуть количество обновленных студентов
+const students = await prisma.student.findMany({
+        include: {
+            person: true
+        }
+    })
+
+    const matchingStudents = students.filter(student =>
+        student.person.name.includes(oldPattern)
+    )
+
+    for (const student of matchingStudents) {
+        await prisma.person.update({
+            where: {
+                id: student.personId
+            },
+            data: {
+                name: student.person.name.replaceAll(oldPattern, newPattern)
+            }
+        })
+    }
+
+    return matchingStudents.length
 }
