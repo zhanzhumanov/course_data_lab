d7667eb9ae3be03b1a59f154d352085fcd2bb848
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:19:26 +0600

    02

--- src/prisma/prisma02.ts
+++ src/prisma/prisma02.ts
@@ -4,5 +4,11 @@ export async function find_courses_after_date(date: Date) {
     return await prisma.course.findMany({
     // TODO: Найти все курсы, созданные после указанной даты
     // Вернуть массив курсов
-    })
+  where: {
+            createdAt: {
+                gt: date
+            }
+        }    
+})
+
 }
