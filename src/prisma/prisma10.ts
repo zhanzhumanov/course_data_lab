bc9747f10b68cb586720592cfb2516f1522f7bcb
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:20:16 +0600

    10

--- src/prisma/prisma10.ts
+++ src/prisma/prisma10.ts
@@ -3,4 +3,24 @@ import { prisma } from './prisma_init'
 export async function get_course_average_grade(courseTitle: string): Promise<number | null> {
     // TODO: Найти среднюю оценку по указанному курсу
     // Вернуть среднее значение или null если курс не найден или нет оценок
+     const course = await prisma.course.findFirst({
+        where: {
+            title: courseTitle
+        }
+    })
+
+    if (!course) {
+        return null
+    }
+
+    const result = await prisma.grade.aggregate({
+        where: {
+            courseId: course.id
+        },
+        _avg: {
+            grade: true
+        }
+    })
+
+    return result._avg.grade
 }
