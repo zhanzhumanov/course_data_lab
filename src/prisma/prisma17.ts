236129a024e04d14ffa4c0627172269a28702dc0
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:27:10 +0600

    17

--- src/prisma/prisma17.ts
+++ src/prisma/prisma17.ts
@@ -4,4 +4,13 @@ export async function delete_courses_without_grades() {
     // TODO: Удалить все курсы, у которых нет ни одной оценки
     // Использовать один запрос deleteMany средствами Prisma
     // Вернуть количество удаленных курсов
+const result = await prisma.course.deleteMany({
+        where: {
+            grades: {
+                none: {}
+            }
+        }
+    })
+
+    return result.count
 }
