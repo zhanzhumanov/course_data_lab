bb0a41a4d5fe4dcfa68b9285343db73c0dbfbfa2
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:19:45 +0600

    05

--- src/prisma/prisma04.ts
+++ src/prisma/prisma04.ts
@@ -4,6 +4,15 @@ export async function find_grades_in_range(minGrade: number, maxGrade: number) {
   // TODO: Найти все оценки в диапазоне от minGrade до maxGrade включительно
   // Вернуть массив оценок с включенной информацией о студенте и курсе
   return await prisma.grade.findMany({
-
+ where: {
+      grade: {
+        gte: minGrade,
+        lte: maxGrade,
+      },
+    },
+    include: {
+      student: true,
+      course: true,
+    },
   });
 }
--- src/prisma/prisma05.ts
+++ src/prisma/prisma05.ts
@@ -5,6 +5,18 @@ export async function find_courses_by_title_paginated(
     skip: number, 
     take: number
 ) {
+    return await prisma.course.findMany({
+        where: {
+            title: {
+                contains: titlePattern
+            }
+        },
+        orderBy: {
+            createdAt: 'desc'
+        },
+        skip,
+        take
+    })
     // TODO: Найти курсы, в названии которых содержится указанная подстрока
     // Применить пагинацию (skip, take) и отсортировать по дате создания (новые первыми)
     // Вернуть массив курсов
