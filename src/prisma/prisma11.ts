7ee4d3f14c5fb37ead97010fc94e9708547305b0
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:26:17 +0600

    11

--- src/prisma/prisma11.ts
+++ src/prisma/prisma11.ts
@@ -5,4 +5,34 @@ export async function update_students_email_domain(oldDomain: string, newDomain:
     // Заменить домен на newDomain
     // Вернуть количество обновленных записей
 	// Допускается обновление email с помощью простого цикла с запросами в теле цикла
+const students = await prisma.student.findMany({
+        include: {
+            person: true
+        },
+        where: {
+            person: {
+                email: {
+                    endsWith: oldDomain
+                }
+            }
+        }
+    })
+
+    for (const student of students) {
+        const newEmail = student.person.email.replace(
+            oldDomain,
+            newDomain
+        )
+
+        await prisma.person.update({
+            where: {
+                id: student.person.id
+            },
+            data: {
+                email: newEmail
+            }
+        })
+    }
+
+    return students.length
 }
