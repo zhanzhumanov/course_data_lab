ab019aa8a8efb06ad0979f78fb05a6c65ac09ad4
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:19:32 +0600

    03

--- src/prisma/prisma03.ts
+++ src/prisma/prisma03.ts
@@ -4,5 +4,15 @@ export async function find_students_by_name_pattern(pattern: string) {
     return await prisma.student.findMany({
     // TODO: Найти всех студентов, у которых имя содержит указанную подстроку
     // Вернуть массив студентов с включенной информацией о person
+       where: {
+            person: {
+                name: {
+                    contains: pattern
+                }
+            }
+        },
+        include: {
+            person: true
+        }
     })
 }
