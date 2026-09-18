e3452d7a341524cb1c747e913f7a9cf812487bcb
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:19:16 +0600

    01

--- src/prisma/prisma01.ts
+++ src/prisma/prisma01.ts
@@ -4,6 +4,15 @@ export async function find_students_by_email_domain(domain: string) {
   // TODO: Найти всех студентов, у которых email заканчивается на указанный домен
   // Вернуть массив студентов с включенной информацией о person
   return await prisma.student.findMany({
-
+ where: {
+      person: {
+        email: {
+          endsWith: domain,
+        },
+      },
+    },
+    include: {
+      person: true,
+    },
   });
 }
