c72c90eafaa2eb402458d20d1e671a7b128f3732
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:33:03 +0600

    29

--- src/prisma/prisma29.ts
+++ src/prisma/prisma29.ts
@@ -4,4 +4,26 @@ export async function find_students_with_same_names() {
     // TODO: Найти студентов с одинаковыми именами
     // Использовать обработку в TypeScript
     // Вернуть массив групп студентов с одинаковыми именами
+ const students = await prisma.student.findMany({
+        include: {
+            person: true
+        }
+    })
+
+    const groups = new Map<string, typeof students>()
+
+    for (const student of students) {
+        const name = student.person.name
+        const group = groups.get(name) ?? []
+
+        group.push(student)
+        groups.set(name, group)
+    }
+
+    return Array.from(groups.entries())
+        .filter(([, students]) => students.length > 1)
+        .map(([name, students]) => ({
+            name,
+            students
+        }))
 }
