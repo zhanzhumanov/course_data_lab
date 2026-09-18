3dd3847ec37ffb612e487f21edf1f7d9be3865f0
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 14:23:08 +0600

    19

--- src/mongo/mongo19.ts
+++ src/mongo/mongo19.ts
@@ -26,6 +26,22 @@ export async function get_order_totals(db: Db): Promise<OrderTotal[]> {
 	// TODO: Рассчитать итоговую сумму для каждого заказа с учетом скидки
 	// Поля: product, total (quantity * unitPrice), finalTotal (total - discount)
 	return await db.collection("orders").aggregate([
-
+ {
+            $project: {
+                product: 1,
+                quantity: 1,
+                unitPrice: 1,
+                discount: 1,
+                total: {
+                    $multiply: ["$quantity", "$unitPrice"]
+                },
+                finalTotal: {
+                    $subtract: [
+                        { $multiply: ["$quantity", "$unitPrice"] },
+                        "$discount"
+                    ]
+                }
+            }
+        }
 	]).toArray() as OrderTotal[]
 }
