4873afd9d42ec7de27cde9c43ac773afe09ac604
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:19:55 +0600

    07

--- src/prisma/prisma07.ts
+++ src/prisma/prisma07.ts
@@ -4,6 +4,23 @@ export async function update_student_email(
   studentId: number,
   newEmail: string
 ) {
+  const student = await prisma.student.update({
+    where: {
+      id: studentId
+    },
+    data: {
+      person: {
+        update: {
+          email: newEmail
+        }
+      }
+    },
+    include: {
+      person: true
+    }
+  });
+
+  return student;
   // TODO: Обновить email студента по его ID
   // Вернуть обновленного студента с информацией о person
 }
