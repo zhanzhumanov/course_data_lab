b022ffeb314a3812c3ced74117a250479048c9ab
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 14:22:10 +0600

    11

--- src/mongo/mongo11.ts
+++ src/mongo/mongo11.ts
@@ -14,4 +14,8 @@ export class Course {
 export async function find_courses_in_departments(db: Db, departments: string[]): Promise<Course[]> {
     // TODO: Найти все курсы, принадлежащие любому из указанных отделов
 	return db.collection("courses")
+     .find({
+            department: { $in: departments }
+        })
+        .toArray()
 }
