170e81dacb837baf42f730800a660fa44970c00d
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:32:42 +0600

    25

--- src/prisma/prisma25.ts
+++ src/prisma/prisma25.ts
@@ -4,4 +4,16 @@ export async function update_courses_without_grades_description() {
     // TODO: Обновить описание курсов без оценок на "Нет оценок"
     // Использовать один запрос updateMany средствами Prisma
     // Вернуть количество обновленных курсов
+const result = await prisma.course.updateMany({
+        where: {
+            grades: {
+                none: {}
+            }
+        },
+        data: {
+            description: 'Нет оценок'
+        }
+    })
+
+    return result.count
 }
