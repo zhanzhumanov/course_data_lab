9495fdfee531b082998b134f3d754df77f1ae778
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:32:04 +0600

    23

--- src/prisma/prisma23.ts
+++ src/prisma/prisma23.ts
@@ -4,4 +4,13 @@ export async function delete_students_without_person() {
     // TODO: Удалить студентов, у которых нет связанной записи в person
     // Использовать фильтрацию средствами Prisma
     // Вернуть количество удаленных записей
+   const result = await prisma.student.deleteMany({
+        where: {
+            person: {
+                student: null,
+            },
+        },
+    });
+
+    return result.count;
 }
