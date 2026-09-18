bbccf0dadbb3827cc78aed7b1cc1af29e838e3db
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:31:59 +0600

    22

--- src/prisma/prisma22.ts
+++ src/prisma/prisma22.ts
@@ -4,4 +4,30 @@ export async function find_courses_min_max_average() {
     // TODO: Найти курсы с минимальной и максимальной средней оценкой
     // Использовать обработку в TypeScript (не агрегацию Prisma)
     // Вернуть объект { min: курс, max: курс } с дополнительным полем averageGrade
+const courses = await prisma.course.findMany({
+        include: {
+            grades: true
+        }
+    })
+
+    const coursesWithAverage = courses
+        .filter(course => course.grades.length > 0)
+        .map(course => ({
+            id: course.id,
+            title: course.title,
+            description: course.description,
+            averageGrade:
+                course.grades.reduce((sum, grade) => sum + grade.grade, 0) /
+                course.grades.length
+        }))
+
+    const min = coursesWithAverage.reduce((prev, current) =>
+        current.averageGrade < prev.averageGrade ? current : prev
+    )
+
+    const max = coursesWithAverage.reduce((prev, current) =>
+        current.averageGrade > prev.averageGrade ? current : prev
+    )
+
+    return { min, max }
 }
