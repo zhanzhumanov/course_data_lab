040d0ce79a6d4a4f540cd50879e3ddfec2412d88
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:27:20 +0600

    19

--- src/prisma/prisma19.ts
+++ src/prisma/prisma19.ts
@@ -4,4 +4,31 @@ export async function find_most_popular_course() {
     // TODO: Найти курс с максимальным количеством уникальных студентов
     // Использовать агрегацию средствами Prisma (groupBy)
     // Вернуть объект курса с дополнительным полем studentCount
+  const grouped = await prisma.grade.groupBy({
+        by: ['courseId'],
+        _count: {
+            studentId: true
+        },
+        orderBy: {
+            _count: {
+                studentId: 'desc'
+            }
+        },
+        take: 1
+    })
+
+    if (grouped.length === 0) {
+        return null
+    }
+
+    const course = await prisma.course.findUnique({
+        where: {
+            id: grouped[0].courseId
+        }
+    })
+
+    return {
+        ...course,
+        studentCount: grouped[0]._count.studentId
+    }
 }
