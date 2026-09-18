e9791c088d8d2906e051e51519d8b4125c541f84
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 14:22:22 +0600

    13

--- src/mongo/mongo13.ts
+++ src/mongo/mongo13.ts
@@ -13,7 +13,10 @@ export class User {
 
 export async function add_role_to_user(db: Db, username: string, newRole: string) {
     // TODO: Добавить новую роль пользователю без замены существующих ролей
-	db.collection("users")
+	 await db.collection("users").updateOne(
+        { username: username },
+        { $push: { roles: newRole } }
+    )
 }
 
 
