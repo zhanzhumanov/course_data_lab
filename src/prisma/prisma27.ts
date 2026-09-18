67cf16f77d749fc308e99f82b430b2d93a8a7c85
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:32:53 +0600

    27

--- src/prisma/prisma27.ts
+++ src/prisma/prisma27.ts
@@ -3,4 +3,17 @@ import { prisma } from './prisma_init'
 export async function find_courses_with_excellent_students() {
     // TODO: Найти курсы с количеством студентов, имеющих оценку 5 по этому курсу
     // Вернуть массив курсов с дополнительным полем excellentCount
+const courses = await prisma.course.findMany({
+        include: {
+            grades: true
+        }
+    })
+
+    return courses.map(course => ({
+        id: course.id,
+        title: course.title,
+        description: course.description,
+        createdAt: course.createdAt,
+        excellentCount: course.grades.filter(grade => grade.grade === 5).length
+    }))
 }
