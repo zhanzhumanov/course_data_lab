e867ec5add6ca2190ff81c0b74e809d3535894d3
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:26:59 +0600

    15

--- src/prisma/prisma15.ts
+++ src/prisma/prisma15.ts
@@ -4,4 +4,16 @@ export async function update_low_grades() {
     // TODO: Обновить все оценки ниже 3 на значение 3
     // Использовать один запрос updateMany средствами Prisma
     // Вернуть количество обновленных записей
+const result = await prisma.grade.updateMany({
+        where: {
+            grade: {
+                lt: 3
+            }
+        },
+        data: {
+            grade: 3
+        }
+    })
+
+    return result.count
 }
