d51260535ab6e36e67bc4dc72520b4373d69bcc6
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 15:32:57 +0600

    28

--- src/prisma/prisma28.ts
+++ src/prisma/prisma28.ts
@@ -1,7 +1,16 @@
 import { prisma } from './prisma_init'
 
-export async function delete_students_before_date(date: Date) {
+export async function delete_students_before_date(baseDate: Date) {
     // TODO: Удалить всех студентов, созданных до указанной даты
     // Использовать один запрос deleteMany средствами Prisma
     // Вернуть количество удаленных записей
+  const result = await prisma.student.deleteMany({
+        where: {
+            createdAt: {
+                lt: baseDate,
+            },
+        },
+    });
+
+    return result.count;
 }
