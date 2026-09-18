21fe8cef11b4b9d62b464d7d77fa2745a88b9b2c
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-18 14:22:36 +0600

    14

--- src/mongo/mongo14.ts
+++ src/mongo/mongo14.ts
@@ -13,5 +13,8 @@ export class Project {
 
 export async function remove_member_from_project(db: Db, projectName: string, memberToRemove: string) {
 	// TODO: Удалить указанного участника из команды проекта
-	db.collection("projects")
+	 await db.collection("projects").updateOne(
+        { name: projectName },
+        { $pull: { team: memberToRemove } }
+    )
 }
