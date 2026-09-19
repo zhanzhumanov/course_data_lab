731ed69c4a357e0970aaaa0da2cf153c6f363732
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 14:05:22 +0600

    25

--- src/serial/serial25.ts
+++ src/serial/serial25.ts
@@ -9,7 +9,11 @@ import { SAXParser } from "sax";
 export function countBooks(xml: string): number {
   const parser = new SAXParser(true);
   let count = 0;
-
+ parser.onopentag = (node) => {
+    if (node.name === "book") {
+      count++;
+    }
+  };
   // TODO: Добавьте обработчик события открытия тега
   // Если имя тега - 'book', увеличивайте count
   
