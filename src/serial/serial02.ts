91618ec21be97ec61dc641c51710807a4b45d805
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 13:27:26 +0600

    02

--- src/serial/serial02.ts
+++ src/serial/serial02.ts
@@ -1,11 +1,12 @@
 /* 
 	Модифицируйте класс Team, чтобы массив members корректно восстанавливался как массив объектов User.
 */
-
+import { Type } from "class-transformer";
 import { User } from "./user";
 
 export class Team {
   name: string;
+  @Type(() => User)
   members: User[]; // Требует декоратора
   
   constructor(name: string, members: User[]) {
@@ -14,4 +15,5 @@ export class Team {
   }
   
   listMembers() { return this.members.map(m => m.getFullName()).join(', '); }
+  
 }
