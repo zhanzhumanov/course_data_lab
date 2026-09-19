ff9908614edd40227efd4c7cc3442195be92c484
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 13:27:37 +0600

    04

--- src/serial/serial04.ts
+++ src/serial/serial04.ts
@@ -1,12 +1,14 @@
 /* 
 	Настройте класс Project с помощью декораторов для работы с разными типами вложенных данных.
 */
-
+import { Type } from "class-transformer";
 import  { User } from "./user";
 
 export class Project {
   name: string;
+  @Type(() => User)
   lead: User; // Требует декоратора
+  @Type(() => User)
   developers: User[]; // Требует декоратора
   constructor(name: string, lead: User, developers: User[]) {
 	this.name = name;
