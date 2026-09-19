c7f8c7bc1678ee702a559188e0d18d1f734898c4
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 14:04:48 +0600

    19

--- src/serial/serial19.ts
+++ src/serial/serial19.ts
@@ -5,6 +5,6 @@
 import { select } from 'xpath';
 
 export function findPrimaryButtons(doc: Document): Node[] {
-  const query = ""; // Тут нужно написать XPath запрос
+  const query = "//button[contains(@class, 'primary')]"; // Тут нужно написать XPath запрос
   return select(query, doc) as Node[];
 }
