d3dfb37e5f7cd77c59d156f0680c105d7f3f3968
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 14:05:46 +0600

    29

--- src/serial/serial29.ts
+++ src/serial/serial29.ts
@@ -2,19 +2,46 @@
 	Найдите книгу с ID=2 и верните её название.
 	Пример XML в файле saxXML.ts
 */
-
-import { SAXParser, type QualifiedAttribute } from "sax";
-
+import { SAXParser } from "sax";
 
 export function findBookById(xml: string, targetId: string): string | null {
   const parser = new SAXParser(true);
-  let currentId:string | QualifiedAttribute = '';
-  let currentTitle = '';
+
   let foundBook = false;
   let isTitleTag = false;
+  let currentTitle = "";
+  let result: string | null = null;
   
 // TODO: Реализуйте логику поиска книги по ID и извлечения её названия
-  
+  parser.onopentag = (node) => {
+    if (node.name === "book") {
+      foundBook = node.attributes.id === targetId;
+    }
+
+    if (node.name === "title" && foundBook) {
+      isTitleTag = true;
+      currentTitle = "";
+    }
+  };
+
+  parser.ontext = (text) => {
+    if (isTitleTag) {
+      currentTitle += text;
+    }
+  };
+
+  parser.onclosetag = (name) => {
+    if (name === "title" && foundBook) {
+      isTitleTag = false;
+      result = currentTitle.trim();
+    }
+
+    if (name === "book") {
+      foundBook = false;
+    }
+  };
+
   parser.write(xml).close();
-  return foundBook ? currentTitle.trim() : null;
+
+  return result;
 }
