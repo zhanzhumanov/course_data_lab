d94fa5ed7b3faad807a742de37eb9c06cd46401e
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 13:27:56 +0600

    08

--- src/serial/serial08.ts
+++ src/serial/serial08.ts
@@ -1,30 +1,34 @@
-/* 	
-	Обеспечьте сериализацию и десериализацию объектов классов Circle, Rectangle и Drawing
-*/
+import { Type } from "class-transformer";
 
 export abstract class Shape {
   abstract area(): number;
 }
 
 export class Circle extends Shape {
+  type: "circle" = "circle";
   radius: number;
+
   constructor(radius: number) {
     super();
     this.radius = radius;
   }
+
   area() {
     return Math.PI * this.radius * this.radius;
   }
 }
 
 export class Rectangle extends Shape {
+  type: "rectangle" = "rectangle";
   width: number;
   height: number;
+
   constructor(width: number, height: number) {
     super();
     this.width = width;
     this.height = height;
   }
+
   area() {
     return this.width * this.height;
   }
@@ -33,7 +37,18 @@ export class Rectangle extends Shape {
 export class Drawing {
   name: string;
 
+  @Type(() => Shape, {
+    discriminator: {
+      property: "type",
+      subTypes: [
+        { name: "circle", value: Circle },
+        { name: "rectangle", value: Rectangle },
+      ],
+    },
+    keepDiscriminatorProperty: true,
+  })
   shapes: Shape[];
+
   constructor(name: string, shapes: Shape[]) {
     this.name = name;
     this.shapes = shapes;
