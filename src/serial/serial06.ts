98f3d0fbf7ce76ca4b9e3a2047a5abae010fa75a
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 13:27:46 +0600

    06

--- src/serial/serial06.ts
+++ src/serial/serial06.ts
@@ -1,11 +1,12 @@
 /* 
 	Настройте класс так, чтобы опциональное поле manager восстанавливалось как User или оставалось null.
 */
-
+import { Type } from "class-transformer";
 import { User } from "./user";
 
 export class Employee {
   name: string;
+    @Type(() => User)
   manager: User | null; // Требует декоратора для типа
 
   constructor(name: string, manager: User | null) {
