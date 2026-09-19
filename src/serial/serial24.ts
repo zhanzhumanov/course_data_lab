6e572b5954848dfdb7d5c596d5fb022b8abf9b34
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 14:05:16 +0600

    24

--- src/serial/serial24.ts
+++ src/serial/serial24.ts
@@ -5,6 +5,6 @@
 import { select } from "xpath";
 
 export function findOnlineUsers(doc: Document): Node[] {
-  const query = ""; // Тут нужно написать XPath запрос
+  const query = '//*[@data-type="user" and contains(concat(" ", normalize-space(@class), " "), " online ")]'; // Тут нужно написать XPath запрос
   return select(query, doc) as Node[];
 }
