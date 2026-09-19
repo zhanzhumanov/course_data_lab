74fcce942fb38ec7341a2e59a7616f8aec9b98f8
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 13:28:01 +0600

    09

--- src/serial/serial09.ts
+++ src/serial/serial09.ts
@@ -3,15 +3,20 @@
 */
 
 
+import { Type } from "class-transformer";
+
 export class TreeNode {
   value: string;
-  
+
+  @Type(() => TreeNode)
   children: TreeNode[];
 
   constructor(value: string) {
-	this.value = value;
-	this.children = [];
+    this.value = value;
+    this.children = [];
   }
-  
-  getChildCount() { return this.children.length; }
+
+  getChildCount() {
+    return this.children.length;
+  }
 }
