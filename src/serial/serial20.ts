f312f867ff1d52a5beceb7d9f8f0f365a0f297fc
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 14:04:55 +0600

    20

--- src/serial/serial20.ts
+++ src/serial/serial20.ts
@@ -5,7 +5,7 @@
 import { select } from 'xpath';
 
 export function findMenuListItems(doc: Document): Node[] {
-  const query = ""; // Тут нужно написать XPath запрос
+  const query = "//ul[@class='menu']/li"; // Тут нужно написать XPath запрос
   return select(query, doc) as Node[];
 }
 
