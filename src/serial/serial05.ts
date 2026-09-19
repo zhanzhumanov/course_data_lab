b903e958197bdb669ba57a1262c54c64d1c2d21f
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 13:27:41 +0600

    05

--- src/serial/serial05.ts
+++ src/serial/serial05.ts
@@ -1,11 +1,12 @@
 /* 	
 	Добавьте декоратор для преобразования строки в объект Date.
 */
-
+import { Type } from "class-transformer";
 import { User } from "./user";
 
 export class Session {
   user: User; // Требует декоратора
+  @Type(() => Date)
   loginAt: Date; // Требует декоратора Date
 
   constructor(user: User, loginAt: Date) {
