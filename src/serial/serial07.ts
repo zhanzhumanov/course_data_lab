1b7f9d9ce81d149595d57621faeb84ae9301cb9e
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 13:27:52 +0600

    07

--- src/serial/serial07.ts
+++ src/serial/serial07.ts
@@ -1,13 +1,16 @@
 /* 
 	Добавьте все необходимые декораторы для корректного восстановления сложной структуры.
 */
-
+import { Type } from "class-transformer";
 import { User } from "./user";
 
 export class Course {
   title: string;
+    @Type(() => User)
   instructor: User; // Требует декоратора
+    @Type(() => User)
   students: User[]; // Требует декоратора
+  @Type(() => Date)  
   schedule: Date; // Требует декоратора
 
   constructor(title: string, instructor: User, students: User[], schedule: Date) {
