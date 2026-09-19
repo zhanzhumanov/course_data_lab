45d48e1d141f0252aa12b1d65e5613f9b5c20af3
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 14:05:11 +0600

    23

--- src/serial/serial23.ts
+++ src/serial/serial23.ts
@@ -5,7 +5,7 @@
 import { select } from 'xpath';
 
 export function findFirstParagraph(doc: Document): Node | null {
-  const query = ""; // Тут нужно написать XPath запрос
+  const query = '//p'; // Тут нужно написать XPath запрос
   const result = select(query, doc) as Node[];
   return result.length > 0 ? result[0] : null;
 }
