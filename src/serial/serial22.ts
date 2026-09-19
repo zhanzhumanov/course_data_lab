0bead295dc62e6820edf4fe4ba60662eda2c6f66
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 14:05:06 +0600

    22

--- src/serial/serial22.ts
+++ src/serial/serial22.ts
@@ -5,6 +5,6 @@
 import { select } from "xpath";
 
 export function findErrorElements(doc: Document): Node[] {
-  const query = ""; // Тут нужно написать XPath запрос
+  const query = '//*[contains(@class, "error")]'; // Тут нужно написать XPath запрос
   return select(query, doc) as Node[];
 }
