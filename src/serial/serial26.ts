23c98e58865c417ddaf78b0b7ee7098a8b1be600
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 14:05:27 +0600

    26

--- src/serial/serial26.ts
+++ src/serial/serial26.ts
@@ -9,7 +9,11 @@ import { SAXParser } from "sax";
 export function collectBookIds(xml: string): string[] {
   const parser = new SAXParser(true);
   const ids: string[] = [];
-  
+   parser.onopentag = (node) => {
+    if (node.name === "book") {
+      ids.push(String(node.attributes.id));
+    }
+  };
   // TODO: При открытии тега 'book' добавляйте значение атрибута 'id' в массив ids
 
   
