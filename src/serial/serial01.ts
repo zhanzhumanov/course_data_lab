a5523ffc6a4489eecc9e8c02d60fbb76f73b93d4
Жанжуманов Рахат Касенович <83414@npo.inet>
2026-09-19 13:27:17 +0600

    01

--- src/serial/serial01.ts
+++ src/serial/serial01.ts
@@ -1,12 +1,13 @@
 /* 	 
 	Дан класс Order и сериализованные данные. Добавьте правильный декоратор к полю customer, чтобы метод getOrderInfo() работал.
 */
-
+import { Type } from "class-transformer";
 import { User } from "./user";
 
 export class Order {
   id: number;
   product: string;
+    @Type(() => User)
   customer: User; // Требует декоратора
 
   constructor(
